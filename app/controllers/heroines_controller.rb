eb6511d28726057c9f5f3d08dbc202ecae9884a2
personabe1984 <personabe1984@gmail.com>
2018-08-20 12:35:27 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,29 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.save
+      flash[:notice] = "Heroine was successfully created."
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def heroine_params
+    params[:heroine].permit(:name, :super_name, :power_id)
+  end
 end
