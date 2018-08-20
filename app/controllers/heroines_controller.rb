bf84d55ed2bbcc728bc639c1b78533ccae631f6e
rosatu <rosalina.tu@gmail.com>
2018-08-20 12:28:07 -0400

    init commit

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,48 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+      if @heroine.update(heroine_params)
+        redirect_to @heroine
+      else
+        render :edit
+      end
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+      if @heroine.save
+        redirect_to @heroine
+      else
+        render :new
+      end
+  end
+
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
