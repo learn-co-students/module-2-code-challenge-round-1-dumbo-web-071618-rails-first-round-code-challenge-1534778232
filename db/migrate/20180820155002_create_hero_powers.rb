3aa267f306690acc39b0ed95f993e9856b994194
me <juniorjr083@gmail.com>
2018-08-20 12:50:55 -0400

    done

---
+++ db/migrate/20180820155002_create_hero_powers.rb
@@ -0,0 +1,10 @@
+class CreateHeroPowers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :hero_powers do |t|
+      t.integer :power_id
+      t.integer :heroine_id
+
+      t.timestamps
+    end
+  end
+end
