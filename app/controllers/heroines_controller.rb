8f937d1c7af3b1d9382b6019be898df8a3e6a9af
joalorro <joalorro@gmail.com>
2018-08-20 12:27:30 -0400

    finished w/ bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,39 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def search
+    @query = params[:q]
+    @heroines = Heroine.all.select do |heroine|
+      heroine.power.name.downcase.include?(@query.downcase)
+    end
+    render :index
+  end
+
+  def show
+    set_heroine
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new heroine_params
+
+    if @heroine.save
+      redirect_to heroine_path @heroine
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+    def set_heroine
+      @heroine = Heroine.find params[:id]
+    end
+
+    def heroine_params
+      params.require(:heroine).permit(:name,:super_name,:power_id)
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,13 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    set_power
+  end 
+
+  private
+    def set_power
+      @power = Power.find params[:id]
+    end
 end
