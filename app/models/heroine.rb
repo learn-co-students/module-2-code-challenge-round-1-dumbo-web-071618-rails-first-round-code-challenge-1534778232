79f60a622738ea7793ad563330c4f44209e06bd9
wachka06 <wachka06@gmail.com>
2018-08-20 12:34:29 -0400

    done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :super_name, uniqueness: true
+
+  # def power_name=(name)
+  #   self.power = Power.find_or_create_by(name: name)
+  # end
+  #
+  # def power_name
+  #   self.power ? self.power.name : nil
+  # end
 end
