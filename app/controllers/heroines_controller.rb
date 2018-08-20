e7e82691eee1a3b559b3dd80ce175d6922df16de
scarlettperry <si.perry15@gmail.com>
2018-08-20 12:33:59 -0400

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
   def index
+    if params[:power]
+      @heroine = Heroine.where('power LIKE ?', "%#{params[:power]}%")
+    else
+      @heroine = Heroine.all
+    end
     @heroines = Heroine.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path(@heroine)
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:power, :name, :super_name, :power_id)
   end
 end
