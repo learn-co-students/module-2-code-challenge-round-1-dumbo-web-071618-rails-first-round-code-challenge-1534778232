6a12416dd491f16b5b64464d4de310c3947aa399
Kyle-Neale <kyle.a.neale@gmail.com>
2018-08-20 12:21:32 -0400

    Done but want to implement search bar

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,39 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine =  Heroine.find(params[:id])
+  end
+
+  # def search
+  #   @heroines = Heroine.all
+  #   power = Power.find_by(name: params[:q])
+  #   @heroines.select do |heroine|
+  #     heroine.power == power
+  #   end
+  # end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      flash[:notice] = "Action performed successfully"
+
+      redirect_to heroine_path(@heroine)
+    else
+
+      redirect_to new_heroine_path(@heroine)
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit!
+  end
 end
